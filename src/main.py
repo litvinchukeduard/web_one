3d9330975a22c14a69712e51f8dc60e309094c14
Dmytro Tarasenko <ug.dotenko@gmail.com>
2024-02-04 13:00:55 +0200

    Answer class

--- src/main.py
+++ src/main.py
@@ -1,3 +1,20 @@
+class Answer:
+    def __init__(self,
+                 id_: str,
+                 text: str,
+                 is_correct: bool):
+        self.id_ = id_
+        self.text = text
+        self.is_correct = is_correct
+
+    def __str__(self):
+        return (f"Answer: id {self.id_}, text {self.text},"
+                + " is_correct {self.is_correct}")
+
+    def __repr__(self):
+        return (f"Answer: id {self.id_}, text {self.text},"
+                + " is_correct {self.is_correct}")
+
 
 if __name__ == '__main__':
     print("Hello world!")
