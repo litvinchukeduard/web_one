adffee7b8fb8f65b219a8601859498945242a3f1
ElenaPolkhovaS <148061037+ElenaPolkhovaS@users.noreply.github.com>
2024-02-04 13:18:51 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,18 @@
 
+class UserAnswer:
+	def __init__(self, user_id, question_id, answer_id):
+		self.user_id = user_id
+		self.question_id = question_id
+		self.answer_id = answer_id
+              
+		
+    def __str__(self):
+        return f''
+	
+	
+
+
+
+
 if __name__ == '__main__':
     print("Hello world!")
