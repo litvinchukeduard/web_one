48f1d567239262e3b319eb9d69fdda6ed6029820
Dmytro Tarasenko <ug.dotenko@gmail.com>
2024-02-04 13:22:28 +0200

    Question class

--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Answer:
     def __init__(self,
                  id_: str,
@@ -14,6 +17,33 @@ class Answer:
     def __repr__(self):
         return (f"Answer: id {self.id_}, text {self.text},"
                 + " is_correct {self.is_correct}")
+
+
+class Question:
+    def __init__(self,
+                 id_: str | int,
+                 text: str,
+                 answers: List[Answer]):
+        self.id_ = id_
+        self.text = text
+        self.answers = answers
+
+    def __str__(self):
+        answers_id = [i.id_ for i in self.answers]
+        return (f"Question: id {self.id_}, text {self.text}"
+                + f"answers {','.join(answers_id)}")
+
+    def __repr__(self):
+        answers_text = [i.text for i in self.answers]
+        return (f"Question: id {self.id_}, text {self.text}"
+                + f"answers {','.join(answers_text)}")
+
+    def add_answer(self, answer: Answer):
+        self.answers.append(answer)
+
+    def del_answer(self, answer: Answer):
+        for index, answer in enumerate(self.answers):
+            pass
 
 
 if __name__ == '__main__':
